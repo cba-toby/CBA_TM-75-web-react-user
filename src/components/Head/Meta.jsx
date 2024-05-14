b999f8ac6e55d222a2c9f6bfd3cec852e8443537
Phan Thế Trung (Toby) <toby@cybridge.jp>
2024-05-14 15:36:36 +0700

    Feature SEO json ld

--- src/components/Head/Meta.jsx
+++ src/components/Head/Meta.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { JsonLd } from "react-schemaorg";
+import { helmetJsonLdProp } from "react-schemaorg";
 
 function Meta({ title, description }) {
+  const schemaData = {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting", // Ví dụ loại từ Schema.org
+    name: "TobyBlog.", // Tên trang web
+    url: "https://tobyblog.click", // URL trang web
+    author: {
+      "@type": "Organization", // Loại tác giả là tổ chức
+      name: "Trung Phan", // Tên tổ chức sở hữu trang web
+    },
+    description:
+      "Trang web chia sẽ kiến thức về lập trình web và công cụ tiện ích", // Mô tả trang web
+    mainEntity: [
+      // Mảng các bài viết hoặc chủ đề chính
+      {
+        "@type": "Article", // Loại nội dung là bài viết
+        headline: "Senimar - Setup Lumen trên AWS", // Tiêu đề bài viết 1
+        url: "https://tobyblog.click/blog-single/setup-apache-nginx-on-aws", // URL bài viết 1
+        // ... các thuộc tính khác cho bài viết 1
+      },
+    ],
+  };
   return (
-    <Helmet>
-      <title>{title}</title>
-      {description && <meta name="description" content={description} />}
-    </Helmet>
+    <>
+      <Helmet>
+        <title>{title}</title>
+        {description && <meta name="description" content={description} />}
+      </Helmet>
+      <Helmet
+        script={[
+          helmetJsonLdProp(schemaData), // Thêm Schema.org vào trang web
+        ]}
+      />
+    </>
   );
 }
 
